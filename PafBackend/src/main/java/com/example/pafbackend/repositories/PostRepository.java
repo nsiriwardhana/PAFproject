56e5daff5e902f9bb7ace10f9bfa2e7e3b42dfe4
nsiriwardhana <nssiriwardhana22@gmail.com>
2025-04-26 11:36:41 +0530

    post repository

---
+++ PafBackend/src/main/java/com/example/pafbackend/repositories/PostRepository.java
@@ -0,0 +1,13 @@
+package com.example.pafbackend.repositories;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.pafbackend.models.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+    List<Post> findByUserId(String userId);
+}
